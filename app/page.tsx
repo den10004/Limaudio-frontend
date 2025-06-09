5b400b239cf16199a05051845f3caca5be025511
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-06-09 17:41:29 +0300

    блок hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Hero from "@/components/Hero/Hero";
+import Hero from "@/components/Hero";
 import Subscription from "@/components/Subscription/Subscription";
 import Brands from "@/components/Brands";
 import PopularArticles from "@/components/PopularArticles";
