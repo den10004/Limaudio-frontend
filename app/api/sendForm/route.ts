62e6ad006ad914578e29a689241a21185de08673
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-06-20 09:48:27 +0300

    изменена фавиконка

--- app/api/sendForm/route.ts
+++ app/api/sendForm/route.ts
@@ -1,45 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+
 /*
-export async function POST(req: NextRequest) {
-  const { hedline, email, name, phone, comment } = await req.json();
-
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.timeweb.ru",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: `"" <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_TO,
-      subject: "Сообщение с сайта Limaudio",
-      text: `
-        Тема: ${hedline}
-        Имя: ${name} 
-        Телефон: ${phone}
-        Email:  ${email || ""}
-        Сообщение: ${comment || ""}`,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Ошибка отправки письма" },
-      { status: 500 }
-    );
-  }
-}*/
-/*
-
 export async function POST(req: NextRequest) {
   const { hedline, email, name, phone, comment } = await req.json();
 
@@ -55,25 +17,25 @@ export async function POST(req: NextRequest) {
     });
 
     let emailText = `
-        Тема: ${hedline}
-        Имя: ${name} 
-        Телефон: ${phone}
-        Сообщение: ${comment || ""}`;
+Тема: ${hedline}
+Имя: ${name}
+Телефон: ${phone}`;
 
     if (email) {
-      emailText = `
-        Тема: ${hedline}
-        Имя: ${name} 
-        Телефон: ${phone}
-        Email: ${email}
-        Сообщение: ${comment || ""}`;
+      emailText += `
+Email: ${email}`;
+    }
+
+    if (comment) {
+      emailText += `
+Сообщение: ${comment}`;
     }
 
     const mailOptions = {
       from: `"" <${process.env.EMAIL_USER}>`,
       to: process.env.EMAIL_TO,
       subject: "Сообщение с сайта Limaudio",
-      text: emailText,
+      text: emailText.trim(),
     };
 
     await transporter.sendMail(mailOptions);
@@ -86,7 +48,8 @@ export async function POST(req: NextRequest) {
       { status: 500 }
     );
   }
-}*/
+}
+*/
 
 export async function POST(req: NextRequest) {
   const { hedline, email, name, phone, comment } = await req.json();
