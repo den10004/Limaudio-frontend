50c0d813526f4be972b78bb0160187a3a3cfdf72
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-06-24 16:52:45 +0300

    дизайн карточки эксперта

--- components/ApplicationForm/index.tsx
+++ components/ApplicationForm/index.tsx
@@ -55,7 +55,27 @@ export default function ApplicationForm({ title }: { title: string }) {
 
       <div className={styles.application__form}>
         <div style={{ position: "relative" }}>
-          <img src="/manager.webp" alt="менеджер" />
+          <svg
+            viewBox="0 0 200 200"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              filter: "blur(50px)",
+              color: " #0055cc",
+              position: "absolute",
+            }}
+          >
+            <path
+              fill="currentColor"
+              d="M35.2,-59.8C49.2,-52.9,66.6,-50.7,71.5,-41.5C76.3,-32.4,68.6,-16.2,65,-2.1C61.3,12,61.7,23.9,59,36.7C56.2,49.5,50.3,63.2,39.9,69.8C29.6,76.3,14.8,75.8,2.9,70.8C-9,65.8,-18,56.2,-25.5,48C-33.1,39.9,-39.2,33,-48.6,25.2C-58,17.4,-70.7,8.7,-71.6,-0.5C-72.5,-9.7,-61.6,-19.5,-52.9,-28.7C-44.3,-37.8,-38,-46.4,-29.6,-56.5C-21.2,-66.7,-10.6,-78.4,0,-78.4C10.6,-78.4,21.2,-66.7,35.2,-59.8Z"
+              transform="translate(100 100)"
+            ></path>
+          </svg>
+
+          <img
+            className={styles.application__img}
+            src="/manager.webp"
+            alt="менеджер"
+          />
           <div className={styles.form__expert}>
             <h3 className="text" style={{ fontWeight: "600" }}>
               Айрат Насыбуллин
