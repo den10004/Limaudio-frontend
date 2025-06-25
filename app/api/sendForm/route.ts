c65fc15135c1d4c5bb5b5c71396dbd389e4fa705
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-06-25 10:21:52 +0300

    sendform

--- app/api/sendForm/route.ts
+++ app/api/sendForm/route.ts
@@ -1,83 +1,37 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-/*
-export async function POST(req: NextRequest) {
-  const { hedline, email, name, phone, comment } = await req.json();
-
-  try {
-    const transporter = nodemailer.createTransport({
-      host: "smtp.timeweb.ru",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    let emailText = `
-Тема: ${hedline}
-Имя: ${name}
-Телефон: ${phone}`;
-
-    if (email) {
-      emailText += `
-Email: ${email}`;
-    }
-
-    if (comment) {
-      emailText += `
-Сообщение: ${comment}`;
-    }
-
-    const mailOptions = {
-      from: `"" <${process.env.EMAIL_USER}>`,
-      to: process.env.EMAIL_TO,
-      subject: "Сообщение с сайта Limaudio",
-      text: emailText.trim(),
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    return NextResponse.json({ success: true });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Ошибка отправки письма" },
-      { status: 500 }
-    );
-  }
-}
-*/
-
 export async function POST(req: NextRequest) {
   const { hedline, email, name, phone, comment } = await req.json();
 
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: 465,
+      port: Number(process.env.EMAIL_PORT) || 465,
       secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      requireTLS: true,
+      tls: {
+        ciphers: "SSLv3",
+      },
     });
 
     let emailText = `
-Тема: ${hedline}
-Имя: ${name}
-Телефон: ${phone}`;
+    Тема: ${hedline}
+    Имя: ${name}
+    Телефон: ${phone}`;
 
     if (email) {
       emailText += `
-Email: ${email}`;
+    Email: ${email}`;
     }
 
     if (comment) {
       emailText += `
-Сообщение: ${comment}`;
+    Сообщение: ${comment}`;
     }
 
     const mailOptions = {
@@ -91,10 +45,7 @@ Email: ${email}`;
 
     return NextResponse.json({ success: true });
   } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Ошибка отправки письма" },
-      { status: 500 }
-    );
+    // console.error(error);
+    return NextResponse.json({ error: error }, { status: 500 });
   }
 }
