8468c752d5a4151e960515108c7ef2efc40283c4
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-07-07 13:30:01 +0300

    дата

--- utils/formatDate.tsx
+++ utils/formatDate.tsx
@@ -5,9 +5,9 @@ export const FormatDate = (dateString: string): string => {
     return "Invalid date";
   }
 
-  return date.toLocaleDateString("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-    year: "numeric",
-  });
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
 };
